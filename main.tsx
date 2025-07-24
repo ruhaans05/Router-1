00cef805c0cf6219c87d45db2581ae7a1f9546b4
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-24 08:49:30 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,1 +1,7 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Hello, Render user!</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
