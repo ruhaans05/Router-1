e217c61c86b911226b2bb0b48be5f0d1239eb8c7
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-24 08:49:04 -0400

    Delete src directory

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-const App = () => <h1>Hello from Router!</h1>
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
